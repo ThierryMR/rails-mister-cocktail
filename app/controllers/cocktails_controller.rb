78f80cbd67867de73b9d397e316a50da09efb9f2
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-01 15:01:55 -0300

    Missing the doses controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,41 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  # def edit
+  # end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    respond_to do |format|
+      if @cocktail.save
+        format.html {redirect_to @cocktail, notice: 'Cocktail esta sendo feito'}
+        format.json {render :show, status: :created, location: @cocktail}
+      else
+        format.html {render :new}
+        format.json {render json: @cocktail.erros, status: :unprocessable_entity }
+      end
+    end
+  end
+
+ private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,33 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(doses_params)
+    respond_to do |format|
+      if @dose.save
+        format.html {redirect_to @dose, notice: 'doses esta sendo feito'}
+        format.json {render :show, status: :created, location: @dose}
+      else
+        format.html {render :new}
+        format.json {render json: @cocktail.erros, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @dose.destroy
+  end
 end
+
+private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def doses_params
+    params.require(:dose).permit(:name)
+  end
