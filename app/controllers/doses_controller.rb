fb0eb98b751a2bfa9e331a484885e91def6c392a
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-02 13:23:49 -0300

    list was added now we need to check if the ingredients is in the list or not

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,11 +6,13 @@ class DosesController < ApplicationController
     # Aqui, o doses passa uma relacao, o build poe um alemento a mais no array
     # @dose = @cocktail.doses.build
     @dose = Dose.new
+    @ingredient = Ingredient.all
   end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(doses_params)
+    @dose.cocktail_id = Cocktail.find(parms[:cocktail_id]).id
       if @dose.save
         redirect_to cocktail_path(@cocktail)
       else
@@ -27,7 +29,7 @@ end
 private
 
   def set_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = Cocktail.find(1)
   end
 
   def doses_params
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
-  # validates :cocktail, presence: true
-  # validates :ingredient, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
   validates :description, presence: true
   validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
